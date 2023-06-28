441d77c957ea307b0dcfe04ac849a1e96044a357
Taebum Kim <ddino99@naver.com>
2023-06-28 20:48:38 +0900

    Animator dev

--- Assets/C#/Controllers/PlayerController.cs
+++ Assets/C#/Controllers/PlayerController.cs
@@ -6,36 +6,89 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     float _speed = 10.0f;
-
-	bool _moveToDest = false;
+    
 	Vector3 _destPos;
+	private PlayerState _state = PlayerState.Idle;
+	
+	public enum PlayerState
+	{
+		Die,
+		Moving,
+		Idle
+	}
 
     void Start()
     {
-		GameManager.InputMng.KeyAction -= OnKeyboard;
-		GameManager.InputMng.KeyAction += OnKeyboard;
-		GameManager.InputMng.MouseAction -= OnMouseClicked;
-		GameManager.InputMng.MouseAction += OnMouseClicked;
+	    GameManager.InputMng.MouseAction -= OnMouseClicked;
+	    GameManager.InputMng.MouseAction += OnMouseClicked;
+		//GameManager.InputMng.KeyAction -= OnKeyboard;
+		//GameManager.InputMng.KeyAction += OnKeyboard;
     }
 
+    void UpdateDie()
+    {
+	    
+    }
+    
+    void UpdateMoving()
+    {
+	    Vector3 dir = _destPos - transform.position;
+	    if (dir.magnitude < 0.0001f)
+	    {
+		    _state = PlayerState.Idle;
+	    }
+	    else
+	    {
+		    float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+		    transform.position += dir.normalized * moveDist;
+		    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 20 * Time.deltaTime);
+	    }
+	    
+	    // 애니메이션
+	    Animator anim = GetComponent<Animator>();
+	    anim.SetFloat("speed", _speed);
+    }
+    
+    void UpdateIdle()
+    {
+	    // 애니메이션
+	    Animator anim = GetComponent<Animator>();
+	    anim.SetFloat("speed", 0);
+    }
+    
     void Update()
     {
-		if (_moveToDest)
-		{
-			Vector3 dir = _destPos - transform.position;
-			if (dir.magnitude < 0.0001f)
-			{
-				_moveToDest = false;
-			}
-			else
-			{
-				float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
-				transform.position += dir.normalized * moveDist;
-				transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 20 * Time.deltaTime);
-			}
-		}
+	    switch (_state)
+	    {
+		    case PlayerState.Die:
+			    UpdateDie();
+			    break;
+		    case PlayerState.Moving:
+			    UpdateMoving();
+			    break;
+		    case PlayerState.Idle:
+			    UpdateIdle();
+			    break;
+	    }
     }
+	
+    void OnMouseClicked(Define.MouseEvent evt)
+    {
+	    if (_state == PlayerState.Die)
+		    return;
+	    
+	    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	    Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
 
+	    RaycastHit hit;
+	    if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
+	    {
+		    _destPos = hit.point;
+		    _state = PlayerState.Moving;
+	    }
+    }
+    
+    /*
     void OnKeyboard()
     {
 		if (Input.GetKey(KeyCode.W))
@@ -64,21 +117,5 @@ public class PlayerController : MonoBehaviour
 
 		_moveToDest = false;
 	}
-
-	void OnMouseClicked(Define.MouseEvent evt)
-	{
-		if (evt != Define.MouseEvent.Click)
-			return;
-
-
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
-
-		RaycastHit hit;
-		if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
-		{
-			_destPos = hit.point;
-			_moveToDest = true;
-		}
-	}
+	*/
 }
