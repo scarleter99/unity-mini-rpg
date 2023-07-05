4b4dd3fd36b6c214c1effda14883cda4bd6c436b
Kim Taebum <ddino99@naver.com>
2023-07-05 14:31:32 +0900

    커서 텍스처 추가

--- Assets/C#/Controllers/PlayerController.cs
+++ Assets/C#/Controllers/PlayerController.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
-	private PlayerStat _stat;
+	private Texture2D _attackIcon;
+	private Texture2D _handIcon;
 	
+	private PlayerStat _stat;
 	private Vector3 _destPos;
-	private PlayerState _state = PlayerState.Idle;
-
+	
 	public enum PlayerState
 	{
 		Die,
@@ -17,9 +19,23 @@ public class PlayerController : MonoBehaviour
 		Idle,
 		Skill
 	}
+	
+	private PlayerState _state = PlayerState.Idle;
+	
+	private enum CursorType
+	{
+		None,
+		Attack,
+		Hand,
+	}
 
-    void Start()
+	private CursorType _cursorType = CursorType.None;
+
+	void Start()
     {
+	    _attackIcon = GameManager.ResourceMng.Load<Texture2D>("Textures/Cursors/Attack");
+	    _handIcon = GameManager.ResourceMng.Load<Texture2D>("Textures/Cursors/Hand");
+	    
 	    _stat = gameObject.GetComponent<PlayerStat>();
 	    
 	    GameManager.InputMng.MouseAction -= OnMouseClicked;
@@ -28,6 +44,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+	    UpdateMouseCursor();
+		    
 	    switch (_state)
 	    {
 		    case PlayerState.Die:
@@ -41,7 +59,7 @@ public class PlayerController : MonoBehaviour
 			    break;
 	    }
     }
-    
+
     void UpdateDie()
     {
 	    
@@ -83,6 +101,33 @@ public class PlayerController : MonoBehaviour
 	    anim.SetFloat("speed", 0);
     }
 
+    void UpdateMouseCursor()
+    {
+	    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	    //Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+
+	    RaycastHit hit;
+	    if (Physics.Raycast(ray, out hit, 100.0f, _layerMask))
+	    {
+		    if (hit.collider.gameObject.layer == (int)Define.Layer.Monster)
+		    {
+			    if (_cursorType == CursorType.Attack)
+				    return;
+			    
+			    Cursor.SetCursor(_attackIcon, new Vector2(_attackIcon.width / 5, 0), CursorMode.Auto);
+			    _cursorType = CursorType.Attack;
+		    }
+		    else
+		    {
+			    if (_cursorType == CursorType.Hand)
+				    return;
+			    
+			    Cursor.SetCursor(_handIcon, new Vector2(_handIcon.width / 3, 0), CursorMode.Auto);
+			    _cursorType = CursorType.Hand;
+		    }
+	    }
+    }
+    
     private int _layerMask = (1 << (int)Define.Layer.Ground) | (1 << (int)Define.Layer.Monster);
     
     void OnMouseClicked(Define.MouseEvent evt)
